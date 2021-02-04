320642af4782f62222e55909d1a4be7ce92b59fa
eito-8428 <toei8428@gmail.com>
2021-02-04 13:27:46 +0900

    html編集

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,138 +1,120 @@
-$(function() {
-    // Peer object
-    const peer = new Peer({
-        key:   "2c059b1e-c304-4d63-90ba-5970acbf60aa",
-        debug: 3,
+const Peer = window.Peer;
+
+(async function main() {
+  const localVideo = document.getElementById('js-local-stream');
+  const joinTrigger = document.getElementById('js-join-trigger');
+  const leaveTrigger = document.getElementById('js-leave-trigger');
+  const remoteVideos = document.getElementById('js-remote-streams');
+  const roomId = document.getElementById('js-room-id');
+  const roomMode = document.getElementById('js-room-mode');
+  const localText = document.getElementById('js-local-text');
+  const sendTrigger = document.getElementById('js-send-trigger');
+  const messages = document.getElementById('js-messages');
+  const meta = document.getElementById('js-meta');
+  const sdkSrc = document.querySelector('script[src*=skyway]');
+
+  meta.innerText = `
+    UA: ${navigator.userAgent}
+    SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
+  `.trim();
+
+  const getRoomModeByHash = () => (location.hash === '#sfu' ? 'sfu' : 'mesh');
+
+  roomMode.textContent = getRoomModeByHash();
+  window.addEventListener(
+    'hashchange',
+    () => (roomMode.textContent = getRoomModeByHash())
+  );
+
+  const localStream = await navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: true,
+    })
+    .catch(console.error);
+
+  // Render local stream
+  localVideo.muted = true;
+  localVideo.srcObject = localStream;
+  localVideo.playsInline = true;
+  await localVideo.play().catch(console.error);
+
+  // eslint-disable-next-line require-atomic-updates
+  const peer = (window.peer = new Peer({
+    key:"2c059b1e-c304-4d63-90ba-5970acbf60aa",
+    debug: 3,
+  }));
+
+  // Register join handler
+  joinTrigger.addEventListener('click', () => {
+    // Note that you need to ensure the peer has connected to signaling server
+    // before using methods of peer instance.
+    if (!peer.open) {
+      return;
+    }
+
+    const room = peer.joinRoom(roomId.value, {
+      mode: getRoomModeByHash(),
+      stream: localStream,
     });
 
-    let localStream;
-    let existingCall;
-
-    peer.on('open', () => {
-        $('#my-id').text(peer.id);
-    step1();
-});
-
-    // Receiving a call
-    peer.on('call', call => {
-        // Answer the call automatically (instead of prompting user) for demo purposes
-        call.answer(localStream);
-    step3(call);
-});
-
-    peer.on('error', err => {
-        alert(err.message);
-    // Return to step 2 if error occurs
-    step2();
-});
-
-    $('#make-call').on('submit', e => {
-        e.preventDefault();
-    // Initiate a call!
-    console.log($('#callto-id').val());
-    const call = peer.call($('#callto-id').val(), localStream);
-    step3(call);
-});
-
-    $('#end-call').on('click', () => {
-        existingCall.close();
-    step2();
-});
-
-    // Retry if getUserMedia fails
-    $('#step1-retry').on('click', () => {
-        $('#step1-error').hide();
-    step1();
-});
-
-    // set up audio and video input selectors
-    const audioSelect = $('#audioSource');
-    const videoSelect = $('#videoSource');
-    const selectors = [audioSelect, videoSelect];
-
-    navigator.mediaDevices.enumerateDevices()
-        .then(deviceInfos => {
-        const values = selectors.map(select => select.val() || '');
-    selectors.forEach(select => {
-        const children = select.children(':first');
-    while (children.length) {
-        select.remove(children);
-    }
-});
-
-    for (let i = 0; i !== deviceInfos.length; ++i) {
-        const deviceInfo = deviceInfos[i];
-        const option = $('<option>').val(deviceInfo.deviceId);
-
-        if (deviceInfo.kind === 'audioinput') {
-            option.text(deviceInfo.label ||
-                'Microphone ' + (audioSelect.children().length + 1));
-            audioSelect.append(option);
-        } else if (deviceInfo.kind === 'videoinput') {
-            option.text(deviceInfo.label ||
-                'Camera ' + (videoSelect.children().length + 1));
-            videoSelect.append(option);
-        }
-    }
-
-    selectors.forEach((select, selectorIndex) => {
-        if (Array.prototype.slice.call(select.children()).some(n => {
-        return n.value === values[selectorIndex];
-})) {
-        select.val(values[selectorIndex]);
-    }
-});
-
-    videoSelect.on('change', step1);
-    audioSelect.on('change', step1);
-});
-
-    function step1() {
-        // Get audio/video stream
-        const audioSource = $('#audioSource').val();
-        const videoSource = $('#videoSource').val();
-        const constraints = {
-            audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
-            video: {deviceId: videoSource ? {exact: videoSource} : undefined},
-        };
-
-        navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-            $('#my-video').get(0).srcObject = stream;
-        localStream = stream;
-
-        if (existingCall) {
-            existingCall.replaceStream(stream);
-            return;
-        }
-
-        step2();
-    }).catch(err => {
-            $('#step1-error').show();
-        console.error(err);
+    room.once('open', () => {
+      messages.textContent += '=== You joined ===\n';
     });
-    }
-
-    function step2() {
-        $('#step1, #step3').hide();
-        $('#step2').show();
-        $('#callto-id').focus();
-    }
-
-    function step3(call) {
-        // Hang up on an existing call if present
-        if (existingCall) {
-            existingCall.close();
-        }
-        // Wait for stream on the call, then set peer video display
-        call.on('stream', stream => {
-            $('#their-video').get(0).srcObject = stream;
+    room.on('peerJoin', peerId => {
+      messages.textContent += `=== ${peerId} joined ===\n`;
     });
 
-        // UI stuff
-        existingCall = call;
-        $('#their-id').text(call.remoteId);
-        call.on('close', step2);
-        $('#step1, #step2').hide();
-        $('#step3').show();
+    // Render remote stream for new peer join in the room
+    room.on('stream', async stream => {
+      const newVideo = document.createElement('video');
+      newVideo.srcObject = stream;
+      newVideo.playsInline = true;
+      // mark peerId to find it later at peerLeave event
+      newVideo.setAttribute('data-peer-id', stream.peerId);
+      remoteVideos.append(newVideo);
+      await newVideo.play().catch(console.error);
+    });
+
+    room.on('data', ({ data, src }) => {
+      // Show a message sent to the room and who sent
+      messages.textContent += `${src}: ${data}\n`;
+    });
+
+    // for closing room members
+    room.on('peerLeave', peerId => {
+      const remoteVideo = remoteVideos.querySelector(
+        `[data-peer-id="${peerId}"]`
+      );
+      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+      remoteVideo.srcObject = null;
+      remoteVideo.remove();
+
+      messages.textContent += `=== ${peerId} left ===\n`;
+    });
+
+    // for closing myself
+    room.once('close', () => {
+      sendTrigger.removeEventListener('click', onClickSend);
+      messages.textContent += '== You left ===\n';
+      Array.from(remoteVideos.children).forEach(remoteVideo => {
+        remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+        remoteVideo.srcObject = null;
+        remoteVideo.remove();
+      });
+    });
+
+    sendTrigger.addEventListener('click', onClickSend);
+    leaveTrigger.addEventListener('click', () => room.close(), { once: true });
+
+    function onClickSend() {
+      // Send message to all of the peers in the room via websocket
+      room.send(localText.value);
+
+      messages.textContent += `${peer.id}: ${localText.value}\n`;
+      localText.value = '';
     }
-});
+  });
+
+  peer.on('error', console.error);
+})();
